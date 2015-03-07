a70ed5564c2b89d2ed73b77d066c5d3cb367b8b2
catarak <ctarakajian@gmail.com>
2015-03-07 16:10:48 -0500

    add emoji scaling

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 var clmTracker;
 var webcam;
-var fps = 60;
-var fpsInterval = 1000/fps;
-var then;
 
 $(function() {
   init();
 
   $("#start-button").click(function(e) {
     clmTracker.startVideo();
-    then = Date.now();
     loop();
   });
 });
@@ -21,16 +17,6 @@ function init() {
 
 function loop() {
   requestAnimationFrame(loop); 
-  var now = Date.now();
-  var elapsed = now - then;
-
   clmTracker.update();
-
-  if (elapsed > fpsInterval) {
-    then = now - (elapsed % fpsInterval);
-
-    //other stuff
-    clmTracker.drawEmoji();
-    clmTracker.clear();
-  }
+  clmTracker.drawEmoji();
 }
--- js/emotion_detection.js
+++ js/emotion_detection.js
@@ -16,8 +16,6 @@ ClmTracker.prototype.init = function() {
 
   this.overlay = document.getElementById('overlay');
   this.overlayCC = this.overlay.getContext('2d');
-
-  this.clear();
 }
 
 ClmTracker.prototype.startVideo = function() {
@@ -26,23 +24,19 @@ ClmTracker.prototype.startVideo = function() {
 }
 
 ClmTracker.prototype.update = function() {
-  this.windowCycles += 1;
-
-  var params = this.ctracker.getCurrentParameters();
-  var _this = this;
-  this.averageParams = _.map(this.averageParams, function(param, i) {
-    return (param + params[i])/_this.windowCycles;
-  });
+  this.currentParams = this.ctracker.getCurrentParameters();
 }
 
 ClmTracker.prototype.drawEmoji = function() {
-  var er = this.ec.meanPredict(this.averageParams);
+  var er = this.ec.meanPredict(this.currentParams);
   //this.maxEmotion = null;
   var maxVal = 0.3;
   if (er.length > 0) {
     var _this = this;
     var maxEr = _.max(er, function(item) { return item.value });
-    this.maxEmotion = maxEr.emotion;
+    if (maxEr.value > maxVal) {
+      this.maxEmotion = maxEr.emotion;
+    }
     console.log(this.maxEmotion);
   }
 
@@ -51,13 +45,11 @@ ClmTracker.prototype.drawEmoji = function() {
   var positions = this.ctracker.getCurrentPosition();
   if (positions) {
     //maybe need to not use this.maxEmotiom?
+    var imgWidth = (positions[13][0] - positions[1][0]) * 1.20;
+    var centerX = positions[62][0];
+    var centerY = positions[62][1];
     var img = document.getElementById(this.maxEmotion);
-    this.overlayCC.drawImage(img, positions[62][0] - 80, positions[62][1]- 80);
-    //this.ctracker.draw(this.overlay);
+    this.overlayCC.drawImage(img, centerX - imgWidth/2, centerY - imgWidth/2, imgWidth, imgWidth);
+    this.ctracker.draw(this.overlay);
   }
-}
-
-ClmTracker.prototype.clear = function() {
-  this.windowCycles = 0;
-  this.averageParams = Array.apply(null, Array(24)).map(function() { return 0.0 });
 }
